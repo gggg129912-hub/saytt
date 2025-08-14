ee14b74973968dc5b704a7ccee9b0769abea700a
gggg129912-hub <gggg129912@gmail.com>
2025-08-14 20:37:26 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("year").textContent = new Date().getFullYear();
+
+function openLightbox(src) {
+    document.getElementById("lightbox-img").src = src;
+    document.getElementById("lightbox").style.display = "flex";
+}
+
+function closeLightbox() {
+    document.getElementById("lightbox").style.display = "none";
+}
